7604a8ff4b3fd74f5e869b361dd5ed8029b3ed34
Jaime Medina <jmedina@rockefeller.edu>
2014-07-24 17:44:43 -0400

    Links fixed

--- MDneeded/common/getsize.tcl
+++ MDneeded/common/getsize.tcl
@@ -5,26 +5,22 @@ proc dimension {atomselection} {
 	set minmax [measure minmax $atomselection]
 	set min [lindex $minmax 0]
 	set max [lindex $minmax 1]
-	puts "x [expr [lindex $max 0] - [lindex $min 0]]"
-	puts "y [expr [lindex $max 1] - [lindex $min 1]]"
-	puts "z [expr [lindex $max 2] - [lindex $min 2]]"
+	set outfile [open "../phase1/COORDS" w]
+	puts $outfile "cellBasisVector1   [expr [lindex $max 0] - [lindex $min 0]] 0 0"
+	puts $outfile "cellBasisVector2   0 [expr [lindex $max 1] - [lindex $min 1]] 0"
+	puts $outfile "cellBasisVector3   0 0 [expr [lindex $max 2] - [lindex $min 2]]"
 	set centercoor [measure center $atomselection]
 	set x [lindex $centercoor 0]
 	set y [lindex $centercoor 1]
 	set z [lindex $centercoor 2]
 	
-	puts "center [format "%.3f %.3f %.3f" $x $y $z]"
+	puts $outfile "cellOrigin         [format "%.3f %.3f %.3f" $x $y $z]"
+	puts $outfile "wrapAll            on"
+	close $outfile
 }
 
 
 set everyone [atomselect top all]
 dimension $everyone
 	
-#set protein [atomselect 0 "segid PROT LIG"]
-#dimension $protein
-
-x 156.8820037841797
-y 158.28699493408203
-z 86.74300003051758
-center -0.424 0.217 0.822
 
